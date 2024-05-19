038aafa4a5ba89b698b0b80388d02539f51496ba
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-19 17:45:57 +0000

    p2 10

---
+++ Java I/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+
+        while(true){
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if ( number == 0){
+                System.out.println("Sum of the numbers: " + sum);
+                break;
+                
+            }
+
+            if ( number != 0){
+                sum += number;
+                continue;
+            }
+            
+        }
+
+    }
+}
