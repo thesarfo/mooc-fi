4ec492e58f72f26cca2b0c8948126d41920b025d
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-01 09:55:15 +0000

    p3 14

---
+++ Java I/part03-Part03_14.OnTheList/src/main/java/OnTheList.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class OnTheList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println("Search for? ");
+        String another = String.valueOf(scanner.nextLine());
+
+        if (list.contains(another)) {
+            System.out.println(another + " was found!");
+        } else {
+            System.out.println(another + " was not found!");
+        }
+
+    }
+}
