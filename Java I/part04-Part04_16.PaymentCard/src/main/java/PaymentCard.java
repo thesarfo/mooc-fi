8625497296fc7215b51a4a7c00ba181f981d39a7
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-06-29 15:59:21 +0000

    p4 16

---
+++ Java I/part04-Part04_16.PaymentCard/src/main/java/PaymentCard.java
@@ -0,0 +1,38 @@
+public class PaymentCard {
+
+    private double balance;
+
+    public PaymentCard(double openingBalance) {
+        this.balance = openingBalance;
+    }
+
+    public String toString() {
+        return "The card has a balance of " + this.balance + " euros";
+    }
+
+    public void eatAffordably() {
+        if (this.balance >= 2.60) {
+            this.balance -= 2.60;
+        }
+    }
+
+    public void eatHeartily() {
+        if (this.balance >= 4.60) {
+            this.balance -= 4.60;
+        }
+    }
+
+    public void addMoney(double amount) {
+        double total;
+        double overage;
+        if (amount > 0) {
+            total = this.balance + amount;
+            overage = total - 150;
+            if (total > 150.00) {
+                total -= overage;
+            }
+            this.balance = total;
+        }
+    }
+
+}
